19277fc0c112e4eb18dccf25049f4361696844c5
Meghashhreem <105379984+Meghashhreem@users.noreply.github.com>
2022-09-21 11:33:08 +0530

    x=int(input()) y=int(input()) sum1=0 sum2=0 for i in range(1,x):     if x%i==0:         sum1+=i for j in range(1,y):     if y%j==0:         sum2+=j if(sum1==y and sum2==x):     print('Amicable') else:     print('Not Amicable')

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,10 +1,14 @@
-n=int(input())
-s=0
-for i in range(1,n):
-    if (n%i==0):
-        s+=i
-m=int(input())
-if (s==m):
-    print("Amicable")
-else:
-    print("Not Amicable")
+x=int(input())  
+y=int(input())  
+sum1=0  
+sum2=0  
+for i in range(1,x):  
+    if x%i==0:  
+        sum1+=i  
+for j in range(1,y):  
+    if y%j==0:  
+        sum2+=j  
+if(sum1==y and sum2==x):  
+    print('Amicable')  
+else:  
+    print('Not Amicable') 
