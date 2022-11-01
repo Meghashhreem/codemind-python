80e02b66ffe5f7646b68b34ce7b6e0aacfdf385e
Meghashhreem <105379984+Meghashhreem@users.noreply.github.com>
2022-11-01 09:53:49 +0530

    n=int(input()) Temp=n s=0 for i in range(1,n):     if (n%i==0):         s+=i if (s==Temp):     print("True") else:     print("False")

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,9 +1,10 @@
-N=int(input())
+n=int(input())
+Temp=n
 s=0
-for i in range(1,N):
-    if (N%i==0):
+for i in range(1,n):
+    if (n%i==0):
         s+=i
-if (s==N):
+if (s==Temp):
     print("True")
 else:
     print("False")
